1adfbcfb142a298619eebb041d7ca737c9fe8da4
sulianto.sulaiman <sulianto.sulaiman@TRAINING016.optimum.net>
2019-08-02 16:46:37 +0800

    COMMIT DONE, 02082019, 1646 HRS

--- CLT-Batch6/CLT-Batch6/src/jdbc/Database01.java
+++ CLT-Batch6/CLT-Batch6/src/jdbc/Database01.java
@@ -1,6 +1,7 @@
 package jdbc;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -14,15 +15,48 @@ public class Database01 {
 		//Execute a query
 		System.out.println("Creating table in a given database...");
 		Statement st = con.createStatement();
-		
+	
 		//create table
+		/*
 		String sql = "CREATE TABLE students " +
 		       "(mobile INTEGER not NULL, " +
 			   " name VARCHAR(25), " +
 		       " email varchar(35), " +
 			   " PRIMARY KEY (mobile))";
 		st.executeUpdate(sql); // for saving purpose
-		System.out.println("Created table in a given database...");
+		System.out.println("Created table in a given database...");*/
+		
+		
+		/*
+		// insert record
+		String sql = "INSERT INTO students " +
+		            "VALUES (676091111, 'Amit', 'amit@gmail.com')";
+		st.executeUpdate(sql);
+		
+		System.out.println("Data Inserted Successfully..");
+		*/
+		
+		// delete a record
+		String sql = "DELETE FROM students " +
+		     "WHERE mobile = 676091111";
+		  st.executeUpdate(sql);
+		  System.out.println("Record Deleted from table Successfully..");
+		 
+		
+		/*
+		//Retrieve Data from Table
+		String sql = "SELECT mobile, name, email FROM students";
+		ResultSet rs = st.executeQuery(sql);
+		
+		//STEP 5: Extract Data from Table
+		System.out.println("\n\n");
+		System.out.println("Mobile\t\t" + "Name" + "\t\t" + "Email");
+		System.out.println("===================================================================");
+		
+		while(rs.next()){
+		   System.out.println(rs.getInt(1)+"\t"+rs.getString(2)+"\t\t"+rs.getString(3));
+		}
+		*/
 	}
 
 }
